0c563b96a03fc70a6f8930c85411958225274838
sapna84 <sapna842004@gmail.com>
2026-07-23 12:18:29 +0530

    changes in duration of hero banners

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -142,7 +142,7 @@ const [currentSlide, setCurrentSlide] = useState(0);
 useEffect(() => {
   const timer = setInterval(() => {
     setCurrentSlide((prev) => (prev + 1) % slides.length);
-  }, 280000);
+  }, 3900);
 
   return () => clearInterval(timer);
 }, []);
